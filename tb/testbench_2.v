531396f5a46eaa913f3a643094826f0aec32abb8
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 22:48:33 +0530

    Update and rename testbench_2.v to tb/testbench_2.v

--- tb/testbench_2.v
+++ tb/testbench_2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.06.2026 19:50:05
-// Design Name: 
-// Module Name: testbench_2
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_2;
     reg clk = 1'b0;
