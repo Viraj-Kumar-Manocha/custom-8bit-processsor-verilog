023ddcae172e26ebf22e26353dde88e16e738bf8
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 21:45:29 +0530

    .

--- rtl/reg_file.v
+++ rtl/reg_file.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 24.06.2026 02:27:25
-// Design Name: 
-// Module Name: reg_file
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module reg_file #(parameter WIDTH = 8,
                   parameter DEPTH = 32,
