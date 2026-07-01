2a720ad0515e3ad7062d62ff68a54dd97caf0943
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 19:40:15 +0530

    .

--- rtl/data_memory_4kB.v
+++ rtl/data_memory_4kB.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.06.2026 19:46:54
-// Design Name: 
-// Module Name: data_memory_4kB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module data_memory_4kB(input clk,
                        input rst,
