e47c0d8f24c044d0044742d569ce967678a1af5e
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 21:58:55 +0530

    .

--- tb/testbench_datapath.v
+++ tb/testbench_datapath.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.06.2026 22:19:07
-// Design Name: 
-// Module Name: testbench_datapath
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_datapath;
 
