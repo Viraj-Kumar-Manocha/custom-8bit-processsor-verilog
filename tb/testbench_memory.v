817b69e25813c1cc1e82b70d2810c60f4b8c5df8
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 21:59:58 +0530

    .

--- tb/testbench_memory.v
+++ tb/testbench_memory.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.06.2026 02:41:31
-// Design Name: 
-// Module Name: testbench_memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_memory;
 reg clk = 1'b0;
