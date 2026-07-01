97d931ab9a8b90ee8f00c4b2037d5b41e5bcd9f8
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 21:45:09 +0530

    .

--- rtl/program_counter.v
+++ rtl/program_counter.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.06.2026 03:43:22
-// Design Name: 
-// Module Name: program_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module program_counter(input clk,
                        input rst,
