3aaac03898956f656a22c5bc41275277ae220bf5
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 19:39:43 +0530

    Update and rename alu.v to rtl/alu.v

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.06.2026 03:47:56
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu(input [7:0] a,
            input [7:0] b,
