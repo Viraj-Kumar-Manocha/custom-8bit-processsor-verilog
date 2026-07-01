b7de608863c8053c7c7d31f0c51621df188caf19
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 19:40:37 +0530

    .

--- rtl/datapath.v
+++ rtl/datapath.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.06.2026 02:51:44
-// Design Name: 
-// Module Name: datapath
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module datapath(input clk,
                 input rst,
