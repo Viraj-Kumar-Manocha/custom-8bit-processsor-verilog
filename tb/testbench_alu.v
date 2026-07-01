2b3daefc05c0680fa8efcb1b80e8e74123116a4f
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 21:58:27 +0530

    .

--- tb/testbench_alu.v
+++ tb/testbench_alu.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.06.2026 03:43:48
-// Design Name: 
-// Module Name: testbench_alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_alu;
 reg [7:0] a,b;
