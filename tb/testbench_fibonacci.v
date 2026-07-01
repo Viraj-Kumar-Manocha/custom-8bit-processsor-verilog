321d90005e3530067d5a47fd4cbcebcaf322f1a4
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 21:59:17 +0530

    .

--- tb/testbench_fibonacci.v
+++ tb/testbench_fibonacci.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.06.2026 21:08:06
-// Design Name: 
-// Module Name: testbench_fibonacci
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module testbench_fibonacci;
 reg clk = 1'b0;
