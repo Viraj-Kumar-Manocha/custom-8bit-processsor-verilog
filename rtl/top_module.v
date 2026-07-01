4fb13af1952a862634dec2ab7c8267f1f061e294
Viraj Kumar <kumar.viraj2019@gmail.com>
2026-07-01 21:45:49 +0530

    .

--- rtl/top_module.v
+++ rtl/top_module.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.06.2026 04:00:42
-// Design Name: 
-// Module Name: top_module
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module top_module( input clk,
                    input rst,
